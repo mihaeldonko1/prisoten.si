1af072283d45c7ee1f849dedf17a541535499366
mihad <miha.donko@gmail.com>
2024-04-30 10:31:38 +0200

    Sprememba razvijalskega okolja v ReactNative

--- prisoten_sem/android/app/src/main/kotlin/com/example/prisoten_sem/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.prisoten_sem
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- prisoten_sem/lib/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:prisoten_sem/screens/auth/login.dart';
-import 'screens/home/home_screen_user.dart'; 
-import 'screens/settings/settings.dart';
-
-final Map<String, WidgetBuilder> appRoutes = {
-  '/': (context) => LoginScreen(),
-  '/settings': (context) => SettingsScreen(), 
-};
