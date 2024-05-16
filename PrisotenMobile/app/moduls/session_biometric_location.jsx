639eb0063212ceed4084a6396b44e5444913be03
NikkaC <nik.kac@student.um.si>
2024-05-16 15:11:02 +0200

    Button color change on true

--- PrisotenMobile/app/moduls/session_biometric_location.jsx
+++ PrisotenMobile/app/moduls/session_biometric_location.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Animated } from 'react-native';
 import { router } from 'expo-router';
 import Styles from './Styles';
 
@@ -8,8 +8,14 @@ function session_biometric_location() {
     const imageSourceFingerprint = require('../../assets/fingerprint.png');
     const imageSourceLocation = require('../../assets/location.png');
 
+    //States for check-ups
     const [fingerprintPressed, setFingerprintPressed] = useState(false);
     const [locationPressed, setLocationPressed] = useState(false);
+
+    //States for button colors
+    const [fingerprintButtonColor, setFingerprintButtonColor] = useState(new Animated.Value(0)); 
+    const [locationButtonColor, setLocationButtonColor] = useState(new Animated.Value(0)); 
+
 
     const handleFingreprintPress = () => {
         console.log('Fingerprint pressed!');
@@ -29,14 +35,47 @@ function session_biometric_location() {
                 pathname: '/moduls/session_attendance',
             });
         }
-    }, [fingerprintPressed, locationPressed])
+    }, [fingerprintPressed, locationPressed]);
 
+    useEffect(() => {
+        if (fingerprintPressed) {
+           Animated.timing(fingerprintButtonColor, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+           }).start(); 
+        }
+    }, [fingerprintPressed, fingerprintButtonColor]);
+
+    useEffect(() => {
+        if (locationPressed) {
+           Animated.timing(locationButtonColor, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+           }).start(); 
+        }
+    }, [locationPressed, locationButtonColor]);
+
+
+
+
+    const fingerprintButtonBackgroundColor = fingerprintButtonColor.interpolate({
+        inputRange: [0,1],
+        outputRange: ['white', '#10f52c'],
+    });
+
+    
+    const locationButtonBackgroundColor = locationButtonColor.interpolate({
+        inputRange: [0,1],
+        outputRange: ['white', '#10f52c'],
+    });
 
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-            <TouchableOpacity style={Styles.circulat_button} onPress={handleFingreprintPress}>
+            <TouchableOpacity style={[Styles.circulat_button, {backgroundColor: fingerprintButtonBackgroundColor}]} onPress={handleFingreprintPress}>
                 <View style={Styles.circular_container}>
                     <Image source={imageSourceFingerprint} style={Styles.image_circular} />
                 </View>
@@ -44,7 +83,7 @@ function session_biometric_location() {
 
             <Text style={Styles.margin_vertical}>Pritisnite za identifikacijo</Text>
 
-            <TouchableOpacity style={Styles.circulat_button} onPress={handleLocationPress}>
+            <TouchableOpacity style={[Styles.circulat_button, {backgroundColor: locationButtonBackgroundColor}]} onPress={handleLocationPress}>
                 <View style={Styles.circular_container}>
                     <Image source={imageSourceLocation} style={Styles.image_circular} />
                 </View>
