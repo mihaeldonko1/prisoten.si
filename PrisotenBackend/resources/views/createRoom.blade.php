d48470524a805bb1b6b5ca46da33c7da7ddcf7dc
mihad <miha.donko@gmail.com>
2024-06-24 12:17:52 +0200

    update to create room

--- PrisotenBackend/resources/views/createRoom.blade.php
+++ PrisotenBackend/resources/views/createRoom.blade.php
@@ -1,15 +1,39 @@
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Create room') }}
         </h2>
     </x-slot>
-            <div class="container" >
+        <div id="setupSettings" style="position: fixed; background-color: #808080a8; height: 100vh; width: 100vw;z-index; 10000;display:none">
+            <div class="container" style="margin-top: 50px">
+
+                <div class="create-card text-center" style="background-color: white; border-radius: 10px">
+                    <div class="row justify-content-end">
+                        <img id="close-room-modal" src="{{ asset('cdn/img/75519.png') }}" style="width: 50px;margin-right: 20px;margin-top: 20px;" />
+                    </div>
+                    <h1 class="mb-3 mt-5" style="font-size: 2.5rem;padding-top: 50px;">Create a custom room</h1>
+                    <div class="form-group" style="padding-left: 150px; padding-right: 150px">
+                        <select class="form-select" name="your_select_name" id="your_select_id">
+                        <option value="" selected>Select a classroom...</option>
+                            @foreach($classrooms as $classroom)
+                                <option value="{{ $classroom['location'] }}" data-id="{{ $classroom['id'] }}">{{ $classroom['name'] }}-{{ $classroom['building'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="slider-container">
+                        <label for="slider">Select acceptance range for students:</label>
+                        <input type="range" class="form-control-range" id="slider" min="10" max="300" value="50" oninput="updateValue(this.value)">
+                        <p>Value: <span id="sliderValue">50</span> meters</p>
+                    </div>
+                    <button id="createRoomBtn" class="btn btn-dark mt-3" style="margin-bottom: 50px">Create Room</button><br>
+                </div>
+            </div>
+        </div>
+        <div class="container" >
                 <div class="row">
                     <div class="col-md-3"></div>
                     <div class="col-md-6 text-center" style="background-color:white; min-height: 50vh; margin-top: 30px; border-radius: 5px">
-                        <button id="createRoomBtn" class="btn btn-dark mt-3">Create Room</button><br>
+                        <button id="openCreateRoomModal" class="btn btn-dark mt-3">Create Room</button><br>
                         <div id="timer" class="timer-container" style="display: none;justify-content: center;">
                             <div class="circle">
                                 <svg width="200" height="200">
@@ -31,18 +55,8 @@
                 </div>
             </div>
         </div>
-
-        
-
-
-
-
-
-
-
-
     </div>
-    
+
 
 <script>
     const socketUrl = @json(config('app.socket_url'));
@@ -67,51 +81,40 @@
     document.getElementById('openCreateRoomModal').addEventListener('click', function() { 
         document.getElementById('setupSettings').style.display = "block";
     });
-const socketUrl = @json(config('app.socket_url'));
-function generateQR(code){
-    QRCode.toCanvas(document.getElementById('qrcode'), code, { errorCorrectionLevel: 'H' }, function (error) {
-            if (error) console.error(error);
-            console.log('QR code generated successfully!');
-        });
-}
 
     document.getElementById('createRoomBtn').addEventListener('click', function() {
         const socket = new WebSocket(socketUrl);
-document.getElementById('createRoomBtn').addEventListener('click', function() {
-    
-    console.log(socketUrl);
-    const socket = new WebSocket(socketUrl);
+
+        socket.onopen = function() {
+            console.log('WebSocket connection established');
+            createMessage(socket);
+        };
+
+        
+        socket.onmessage = function(event) {
+            const dataObject = JSON.parse(event.data);
+            const actionValue = dataObject.action;
+            console.log(dataObject);
+            if(actionValue == "created"){
+                    let genRoomCode = dataObject.roomCode;
+                    roomCrafted = genRoomCode;
+
+                    axios.post('/schedule-close-websocket', { code: genRoomCode, timeLeft: 15 })
+                    .then(function(response) {
+                        console.log('Close WebSocket job scheduled:', response.data);
+                    })
+                    .catch(function(error) {
+                        console.error('Error scheduling close WebSocket job:', error);
+                    });
 
 
-    socket.onopen = function() {
-        console.log('WebSocket connection established');
-        createMessage(socket);
-    };
-    
-    socket.onmessage = function(event) {
-        const dataObject = JSON.parse(event.data);
-        const actionValue = dataObject.action;
-        console.log(dataObject);
-        if(actionValue == "created"){
-                var genRoomCode = dataObject.roomCode;
-
-
-                axios.post('/schedule-close-websocket', { code: genRoomCode, timeLeft: 15 })
-                .then(function(response) {
-                    console.log('Close WebSocket job scheduled:', response.data);
-                })
-                .catch(function(error) {
-                    console.error('Error scheduling close WebSocket job:', error);
-                });
-
-
-                axios.post('/create-room', { code: genRoomCode, id: '{{ Auth::user()->id }}' })
-                .then(function(response) {
-                    console.log('created room x');
-                })
-                .catch(function(error) {
-                    console.error('error room x');
-                });
+                    axios.post('/create-room', { code: genRoomCode, id: '{{ Auth::user()->id }}', classroom: dataId })
+                    .then(function(response) {
+                        console.log('created room x');
+                    })
+                    .catch(function(error) {
+                        console.error('error room x');
+                    });
 
 
                     startTimer();
@@ -136,83 +139,88 @@ document.getElementById('createRoomBtn').addEventListener('click', function() {
                 });
             }
         };
-                startTimer();
-                document.getElementById("timer").style.display = "grid";
-                document.getElementById("room-code-txt").style.display = "block";
-                document.getElementById('createRoomBtn').style.display = "none";
-                document.getElementById("roomkey").innerHTML = genRoomCode;
-                generateQR(genRoomCode);
-        }else if(actionValue == "user_joined"){
-            document.getElementById("user_list").style.display = "block"
-            addUser(dataObject.name, dataObject.email, `{{ asset('cdn/img/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg') }}`);
-        }
-    };
 
-    socket.onerror = function(error) {
-        console.error('WebSocket error: ', error);
-    };
+        socket.onerror = function(error) {
+            console.error('WebSocket error: ', error);
+        };
 
-    socket.onclose = function(event) {
-        console.log('WebSocket connection closed', event);
-    };
-});
+        socket.onclose = function(event) {
+            console.log('WebSocket connection closed', event);
+        };
+    });
 
-function addUser(name, email, avatarSrc) {
-    const userDiv = document.createElement('div');
-    userDiv.className = 'col-md-6 d-flex justify-content-center align-items-center';
+    function addUser(name, email, avatarSrc) {
+        const userDiv = document.createElement('div');
+        userDiv.className = 'col-md-6 d-flex justify-content-center align-items-center';
 
-    const userImage = document.createElement('img');
-    userImage.src = avatarSrc;
-    userImage.id = 'user-logo';
-    userImage.alt = 'User avatar';
-    userImage.style.height = '20px';
-    userImage.style.width = '20px';
-    userImage.style.marginRight = '10px';
+        const userImage = document.createElement('img');
+        userImage.src = avatarSrc;
+        userImage.id = 'user-logo';
+        userImage.alt = 'User avatar';
+        userImage.style.height = '20px';
+        userImage.style.width = '20px';
+        userImage.style.marginRight = '10px';
 
-    const userInfoDiv = document.createElement('div');
-    userInfoDiv.className = 'user-info';
+        const userInfoDiv = document.createElement('div');
+        userInfoDiv.className = 'user-info';
 
-    const userName = document.createElement('h4');
-    userName.textContent = name;
-    userInfoDiv.appendChild(userName);
+        const userName = document.createElement('h4');
+        userName.textContent = name;
+        userInfoDiv.appendChild(userName);
 
 
-    const userEmail = document.createElement('p');
-    userEmail.textContent = email;
-    userInfoDiv.appendChild(userEmail);
+        const userEmail = document.createElement('p');
+        userEmail.textContent = email;
+        userInfoDiv.appendChild(userEmail);
 
-    userDiv.appendChild(userImage);
-    userDiv.appendChild(userInfoDiv);
+        userDiv.appendChild(userImage);
+        userDiv.appendChild(userInfoDiv);
 
-    document.getElementById('joined_users').appendChild(userDiv);
-}
+        document.getElementById('joined_users').appendChild(userDiv);
+    }
 
-// Example usage
-function createMessage(socket) {
-    const message = {
-        "action": "create",
-        "name": "Miha Donko",
-        "email": "YOUR_EMAIL@example.com",
-        "location": {
-            "coords": {
-                "accuracy": 100,
-                "longitude": 15.1573861,
-                "altitude": 468.1999816894531,
-                "heading": 0,
-                "latitude": 46.5909305,
-                "altitudeAccuracy": 100,
-                "speed": 0
-            },
-            "mocked": false,
-            "timestamp": 1716478829145
-        },
-        "diameter": 100
-    };
+    function createMessage(socket) {
+            var selectElement = document.getElementById('your_select_id');
+            var selectedOption = selectElement.options[selectElement.selectedIndex];
+            var selectedLocation = selectedOption.value;
+            dataId = selectedOption.getAttribute('data-id');
+            let parsedLocation = JSON.parse(selectedLocation);
 
-    socket.send(JSON.stringify(message));
-}
+            console.log(dataId);
+
+            var sliderElement = document.getElementById('slider');
+            var diameterValue = sliderElement.value;
+
+            console.log(selectedLocation);
+
+        const message = {
+            "action": "create",
+            "name": "{{ Auth::user()->name }}",
+            "email": "{{ Auth::user()->email }}",
+            "location": parsedLocation,
+            "diameter": diameterValue
+        };
+
+        console.log(JSON.stringify(message));
+
+        socket.send(JSON.stringify(message));
+    }
 </script>
 <script>
+        function updateJobTimer(newTime){
+            //TODO implement job timer update
+            console.log("new timer" + newTime);
+            console.log("room code" + roomCrafted);
+            axios.post('/update-close-websocket', { timeLeft: newTime, code: roomCrafted })
+            .then(function(response) {
+                console.log('Close WebSocket job scheduled:', response.data);
+            })
+            .catch(function(error) {
+                console.error('Error scheduling close WebSocket job:', error);
+            });
+        }
+
+
         let timer;
         let totalTime;
         let timeLeft;
@@ -262,9 +270,11 @@ function createMessage(socket) {
                 alert("Timer has already finished.");
                 return;
             }
-            totalTime += 5 * 60; // Add 5 minutes to the total time
-            timeLeft += 5 * 60;  // Add 5 minutes to the remaining time
+            totalTime += 5 * 60;
+            timeLeft += 5 * 60; 
             updateTimerText(timeLeft);
+
+            updateJobTimer(timeLeft);
 
             const foregroundCircle = document.getElementById('foregroundCircle');
             foregroundCircle.style.transition = 'none';
@@ -276,28 +286,12 @@ function createMessage(socket) {
                     foregroundCircle.style.strokeDashoffset = circleLength * (timeLeft / totalTime);
                 });
             });
-            //TO JE DODANO NE DELA!
-            console.log(genRoomCode);
-            axios.post('/edit-room', { code: genRoomCode, id: '{{ Auth::user()->id }}' })
-                .then(function(response) {
-                    console.log('created room x');
-                })
-            axios.post('/schedule-close-websocket', { code: genRoomCode, timeLeft: 15 })
-            .then(function(response) {
-                console.log('Close WebSocket job scheduled:', response.data);
-            })
-            .catch(function(error) {
-                console.error('Error scheduling close WebSocket job:', error);
-            });
         }
 
         function updateTimerText(time) {
             const minutes = Math.floor(time / 60);
             const seconds = time % 60;
             document.getElementById('timerText').textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-            /// TODO TIMER SPREMENI CARBON DATABASE
-
-
         }
     </script>
 </x-app-layout>
